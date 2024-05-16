25c4476a588dcd5a9a34af8866b98377afb2bce2
Pangsuphamat <suphamat.c@rmutl.ac.th>
2024-05-16 16:56:55 +0700

    เพิ่ม

--- lib/screen/my_home_page.dart
+++ lib/screen/my_home_page.dart
@@ -1,10 +1,32 @@
+import 'package:example_1/screen/my_second_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Hello world");
+    return Scaffold(
+      drawer: const Drawer(
+        child: Text("My Drawer"),
+      ),
+      appBar: AppBar(
+        title: const Text("My Home Page"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Text ("Hello World"),
+            ElevatedButton(
+              onPressed: () {
+                 Navigator.push(context, MaterialPageRoute(builder: (context) => const MySecondPage()));
+                 },
+                         child: const Text("ไปยังหน้าที่ 2"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
